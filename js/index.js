fe298c6e978c80ff7c7aa128e76fe580720ef61e
Guilherme-Pro <guilhermecarrijo135@gmail.com>
2024-05-03 01:49:54 +0100

    modificado

--- js/index.js
+++ js/index.js
@@ -3,33 +3,32 @@ const closeButton = document.querySelector('.close');
 const openButton = document.querySelector('.edit__avatar_edit');
 const overlay = document.querySelector('.popup__overlay');
 const popup = document.querySelector('.popup');
-closeButton.addEventListener('click', function() {
-    popup.style.display = 'none';
-});
-openButton.addEventListener('click', function() {
-    popup.style.display = 'flex'; 
-});
-
-
+const newName = document.querySelector('.popup__form_input-name')
+const newJob = document.querySelector('.popup__form_input-job')
+const nameText = document.querySelector('.edit__avatar_title');
+const jobText = document.querySelector('.edit__avater_subtitle');
 const salve = document.querySelector('.popup__form_bnt-submit');
-popup.style.display = 'none';
-salve.addEventListener('click', function(){
-    const newName = document.querySelector('.popup__form_input-name').value;
-    const newJob = document.querySelector('.popup__form_input-job').value;
-    const nameText = document.querySelectorAll('.edit__avatar_title');
-    const jobText = document.querySelectorAll('.edit__avater_subtitle');
-    
-    function atualizarInformacoes() {
-        for (let i = 0; i < nameText.length; i++) {
-            nameText[i].textContent = newName;
-        }
-    
-        for (let i = 0; i < jobText.length; i++) {
-            jobText[i].textContent = newJob;
-        }
-    }
-    
-   
-    
-    atualizarInformacoes();
+
+
+function fecharPopup(){
+    popup.classList.remove("popup__opened")
+}
+closeButton.addEventListener('click', fecharPopup);
+
+
+openButton.addEventListener('click', () => {
+    popup.classList.add("popup__opened")
+    newName.value = nameText.textContent;
+    newJob.value = jobText.textContent;
 });
+
+
+function handleFormSubmit (e) {
+    e.preventDefault();
+    nameText.textContent = newName.value;
+    jobText.textContent = newJob.value;
+    
+    fecharPopup()
+}
+
+salve.addEventListener("click", handleFormSubmit)
