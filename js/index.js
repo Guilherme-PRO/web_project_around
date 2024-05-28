620f3ea10656c93959f918e8c31145b7728de0b3
Guilherme-Pro <guilhermecarrijo135@gmail.com>
2024-05-28 21:55:04 +0100

    comcluido

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 
 const closeButton = document.querySelector('.close');
-const openButton = document.querySelector('.edit__avatar__edit_img');
+const openButton = document.querySelector('.profile__new__edit__avatar_edit');
 const overlay = document.querySelector('.popup__overlay');
 const popup = document.querySelector('.popup');
 const newName = document.querySelector('.popup__form_input-name')
 const newJob = document.querySelector('.popup__form_input-job')
-const nameText = document.querySelector('.edit__avatar__edit_title');
-const jobText = document.querySelector('.edit__avater_subtitle');
+const nameText = document.querySelector('.profile__new__edit__avatar_title');
+const jobText = document.querySelector('.profile__new__edit_subtitle');
 const salve = document.querySelector('.popup__form_bnt-submit');
-const like = document.querySelector('.element__card_like')
+const like = document.querySelector('.gallery__elements__element__card_like')
 
 function fecharPopup(){
     popup.classList.remove("popup__opened")
